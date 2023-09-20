fe62930e2d8c821766ba1024fa8e183c50ab631e
SAF2k <safwathkhan2@gmail.com>
2023-09-20 07:14:09 +0530

    small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
